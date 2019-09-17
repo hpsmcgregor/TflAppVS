728241d404fb49355dc4bdd9158a704d2371f206
Hamish McGregor <hamish.p.mcgregor@btinternet.com>
2019-09-17 08:03:05 +0100

    cleanup - removes unused files and unnecessary usings

--- TflAppVS.Functions/TflApis/ColourHelper.cs
+++ TflAppVS.Functions/TflApis/ColourHelper.cs
@@ -16,17 +16,12 @@ namespace TflAppVS.Functions.TflApis
             string coloursJson = File.ReadAllText("./resources/Colours.json");
             var rgbColours = JsonConvert.DeserializeObject<List<RgbColourDto>>(coloursJson);
 
-            //var tubeColours = JArray.Parse(json);
             foreach (var rgbColour in rgbColours)
             {
                 if (!_tubeColourRGBDictionary.ContainsKey(rgbColour.Id))
                 {
                     _tubeColourRGBDictionary.Add(rgbColour.Id, rgbColour.Rgb);
                 }
-                //_tubeColorRGBDictionary.Add(tubeColor["id"].Value<string>(), new Rgb(
-                //    tubeColor["RGB"]["R"]?.Value<int>() ?? 0,
-                //    tubeColor["RGB"]["G"]?.Value<int>() ?? 0,
-                //    tubeColor["RGB"]["B"]?.Value<int>() ?? 0));
             }
         }
 
